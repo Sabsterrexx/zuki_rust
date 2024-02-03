15fd1f13e4a20ba06301d1c340cf41599d11d4c7
henceiusegentoo <61363222+henceiusegentoo@users.noreply.github.com>
2024-02-03 10:11:04 +0100

    Added image generation requests

--- src/image_request.rs
+++ src/image_request.rs
@@ -1,8 +1,36 @@
 use crate::ZukiInterface;
+use serde::Serialize;
+
+#[derive(Serialize)]
+pub struct ImageRequestBody {
+    prompt: String,
+    model: String,
+    size: String,
+    quality: String,
+    n: u8
+}
 
 impl ZukiInterface {
-    pub async fn image_request(&self, message: &str) -> String {
-        let response = format!("ZukiInterface::image_request({})", message);
-        response
+    pub async fn image_request(&self, model: &str, prompt: &str, size: &str, quality: &str, n: u8) -> serde_json::Value {
+        let body = ImageRequestBody {
+            prompt: String::from(prompt),
+            model: String::from(model),
+            size: String::from(size),
+            quality: String::from(quality),
+            n
+        };
+        
+        let base_extension = "v1";
+        let url = format!("{}/{}/images/generations", self.base_url, base_extension);
+
+        let request = reqwest::Client::new()
+            .post(&url)
+            .header("Authorization", &self.auth)
+            .json(&body)
+            .send()
+            .await
+            .unwrap();
+
+        request.json().await.unwrap()
     }
 }
